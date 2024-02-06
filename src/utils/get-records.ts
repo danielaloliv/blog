821ff94c5db8c32ee8502427c61feb40697b9fb3
Pedro Pupo Sa da Costa <pedro.pupo@ist.utl.pt>
2024-02-06 17:02:21 +0000

    add zod validation

--- src/utils/get-records.ts
+++ src/utils/get-records.ts
@@ -1,14 +1,27 @@
 
+import { z } from "zod";
 import { airtable } from "./airtable";
+
+const postSchema = z.object({
+  ["Title"]: z.string(),
+  ["Lead"]: z.string(),
+  ["Content"]: z.string(),
+  ["Date of post"]: z.string(),
+});
 
 export async function getRecords() {
   const base = airtable.base('appWKgDXDQPkk7xr0');
   const posts = await base('Posts').select().all();
 
-  return posts.map((post) => ({
-    id: post.id,
-    title: post.fields["Title"] as string,
-    content: post.fields["Content"] as string,
-    date : post.fields["Date of post"] as string,
-  }));
+  return posts
+    .map((post) => {
+      const fields = postSchema.parse(post.fields);
+      return {
+        id: post.id,
+        title: fields.Title,
+        content: fields.Content,
+        lead: fields.Lead,
+        date: fields["Date of post"],
+      }
+    });
 }
